479a053719e9b24513f6fd2e151fea07aaa68fe7
Vinicius <vinilean43@gmail.com>
2022-07-14 17:11:37 -0300

    Full API service

--- user-api/src/main/java/com/backend/userapi/controller/UserController.java
+++ user-api/src/main/java/com/backend/userapi/controller/UserController.java
@@ -6,13 +6,19 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.userapi.dto.UserDTO;
 
 @RestController
 public class UserController {
+	
+	public static List<UserDTO> user = new ArrayList<>();
 
 	@GetMapping("/")
 	public String message() {
@@ -24,7 +30,32 @@ public class UserController {
 		return user;
 	}
 	
-	public static List<UserDTO> user = new ArrayList<>();
+	@GetMapping("/users/{cpf}")
+	public UserDTO getUsersFiltro(@PathVariable String cpf) {
+		for(UserDTO userFilter : user) {
+			if(userFilter.getCpf().equals(cpf)) {
+				return userFilter;
+			} 
+		}
+		return null;
+	}
+	
+	@PostMapping("/newUser")
+	UserDTO inserir(@RequestBody UserDTO userDTO) {
+		userDTO.setDateRegister(new Date());
+		return userDTO;
+	}
+	
+	@DeleteMapping("/users/{cpf}")
+	public boolean delete(@PathVariable String cpf) {
+		for(UserDTO userFilter : user) {
+			if(userFilter.getCpf().equals(cpf)) {
+				user.remove(userFilter);
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	@PostConstruct
 	public void initiateList() {
@@ -35,23 +66,23 @@ public class UserController {
 		user1.setAddress("Rua a");
 		user1.setEmail("vinicius@email.com");
 		user1.setTelphone("3532-2254");
-		user1.setDateRegister(new Date());
+		inserir(user1);
 		
 		UserDTO user2 = new UserDTO();
 		user2.setName("Tiago");
-		user2.setCpf("123");
+		user2.setCpf("234");
 		user2.setAddress("Rua b");
 		user2.setEmail("tiago@email.com");
 		user2.setTelphone("2541-6587");
-		user2.setDateRegister(new Date());
+		inserir(user2);
 		
 		UserDTO user3 = new UserDTO();
 		user3.setName("Roberto");
-		user3.setCpf("123");
+		user3.setCpf("345");
 		user3.setAddress("Rua c");
 		user3.setEmail("roberto@email.com");
 		user3.setTelphone("4485-2246");
-		user3.setDateRegister(new Date());
+		inserir(user3);
 		
 		user.add(user1);
 		user.add(user2);
