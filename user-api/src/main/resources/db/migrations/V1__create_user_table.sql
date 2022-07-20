8357152e568d3543b6a1c6d845e4cf0f3268a38b
Vinicius <vinilean43@gmail.com>
2022-07-20 16:23:51 -0300

    postgres

--- user-api/src/main/resources/db/migrations/V1__create_user_table.sql
+++ user-api/src/main/resources/db/migrations/V1__create_user_table.sql
@@ -1,12 +1,11 @@
-
 create schema if not exists users;
 
 create table users.user (
     id bigserial primary key,
-    nome varchar(100) not null,
+    name varchar(100) not null,
     cpf varchar(100) not null,
-    endereco varchar(100) not null,
+    address varchar(100) not null,
     email varchar(100) not null,
-    telefone varchar(100) not null,
-    data_cadastro timestamp not null
+    telephone varchar(100) not null,
+    date_register timestamp not null
 );
